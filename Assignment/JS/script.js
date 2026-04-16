e1fef4dc446056304abd6bd9c9d11d3671fb054e
rahulshek <shekhawat.rd31@gmail.com>
2026-04-16 11:17:16 +0530

    Bill System

---
+++ Assignment/JS/script.js
@@ -0,0 +1,18 @@
+// console.log("hii");
+
+let calBil = (day)=>{
+    let totalBill=1000*(day)
+    if (day<=2){
+        totalBill=totalBill-(totalBill*0.05)
+    }
+    else if (day>2 && day<=10){
+        totalBill=totalBill-(totalBill*0.07)
+    }
+    else {
+        totalBill=totalBill-(totalBill*0.1)
+    }
+    console.log(totalBill);
+}
+
+let stayDay = prompt("Enter the total number of day's : ")
+calBil(stayDay)
